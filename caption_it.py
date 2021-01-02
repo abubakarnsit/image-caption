2242c4bc474f50351ac27a1720f4d9c9bcb2995e
abubakarnsit <62075405+abubakarnsit@users.noreply.github.com>
2021-01-02 15:18:08 +0530

    Add files via upload

--- caption_it.py
+++ caption_it.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:S
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,21 +13,12 @@ from keras.utils import to_categorical
 from keras.layers import Input, Dense, Dropout, Embedding, LSTM
 from keras.layers.merge import add
 
-# In[2]:
-
 model=load_model("model_weights_image_captioning/model_19.h5")
 model._make_predict_function()
 
-# In[3]:
 model_temp= ResNet50(weights="imagenet",input_shape=(224,224,3))
-
-
 model_resnet = Model(model_temp.input,model_temp.layers[-2].output)
 model_resnet._make_predict_function()
-
-
-# In[5]:
-
 
 def preprocess_img(img):
     img = image.load_img(img,target_size=(224,224))
@@ -42,10 +29,6 @@ def preprocess_img(img):
     img = preprocess_input(img)
     return img
 
-
-# In[6]:
-
-
 def encode_image(img):
     img = preprocess_img(img)
     feature_vector = model_resnet.predict(img)
@@ -54,29 +37,11 @@ def encode_image(img):
     #print(feature_vector.shape)
     return feature_vector
 
-
-# In[ ]:
-
-
-
-
-
-# In[7]:
-
-
 with open("word_to_idx.pkl","rb") as w2i:
        word_to_idx= pickle.load(w2i)
 
-
-# In[8]:
-
-
 with open("idx_to_word.pkl","rb") as w2i:
     idx_to_word=pickle.load(w2i)
-
-
-# In[13]:
-
 
 def predict_caption(photo):
     max_len=38
@@ -97,34 +62,12 @@ def predict_caption(photo):
     final_caption = ' '.join(final_caption)
     return final_caption
 
-
-# In[14]:
-
 def caption_this_image(image):
     enc=encode_image(image)
     caption=predict_caption(enc)
     return caption
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
